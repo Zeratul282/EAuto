6fc57a4f3359048b50e9438503fa445c4beb05c3
Zeratul <merlin282@wp.pl>
2016-04-07 15:26:52 +0200

    pom

--- src/main/java/eauto/EAuto.java
+++ src/main/java/eauto/EAuto.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package eauto;
+package main.java.eauto;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
