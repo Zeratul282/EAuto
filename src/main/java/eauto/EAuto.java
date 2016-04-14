0b3144a6c87d9be77542b15aa11de5f50def0f15
Zeratul <merlin282@wp.pl>
2016-04-14 15:47:00 +0200

    test2 jenkins

--- src/main/java/eauto/EAuto.java
+++ src/main/java/eauto/EAuto.java
@@ -22,7 +22,7 @@ public class EAuto extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello Worldzzz'");
+        btn.setText("Say 'Hello Worldz'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
