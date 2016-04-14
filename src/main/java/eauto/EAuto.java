f9762c889d20e7860104280b9f3bc6e556bc8379
JanNowski <ksymilian@gmail.com>
2016-04-14 14:02:43 +0200

    After build v2

--- src/main/java/eauto/EAuto.java
+++ src/main/java/eauto/EAuto.java
@@ -27,7 +27,7 @@ public class EAuto extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("EAuto! ");
             }
         });
         
@@ -36,7 +36,7 @@ public class EAuto extends Application {
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("EAuto");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
